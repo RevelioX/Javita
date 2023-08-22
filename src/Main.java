ed90ac97e521e2f544d82bf800fac1f3a4b4bb8e
RevelioX <brardamariano12@gmail.com>
2023-08-22 17:29:47 -0300

    xddd

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
-import java.sql.SQLOutput;
-
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
         System.out.print("Hello World");
+        File miArchivo = new File("num.txt");
+        System.out.println(miArchivo);
+        try{
+            Scanner scannercito = new Scanner(miArchivo);
+            while(scannercito.hasNextInt()){
+                var num = scannercito.nextInt();
+                System.out.print(num);
+            }
+            scannercito.close();
+
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+
         double decimalote;
         decimalote = 3.5;
         float banana;
@@ -33,10 +49,8 @@ public class Main {
     public Hilito(int a){
         this.value = a;
     }
-    public void run(){
-        for(int i=0;i>=100;i++){
+    public void run() {
+        for (int i = 0; i >= 100; i++) {
             System.out.println(value);
         }
-    }
-
-}
+    } }
